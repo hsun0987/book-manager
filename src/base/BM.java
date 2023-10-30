58dc0971b4cf0f7a1452052bd14466c85b4379d1
MinSun <hsun0987@daum.net>
2023-10-31 00:23:58 +0900

    임시 저장

--- src/base/BM.java
+++ src/base/BM.java
@@ -1,5 +1,6 @@
 package base;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,19 +8,12 @@ import java.util.Scanner;
 public class BM extends BookManager {
 
     private ArrayList<Book> bookList = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     @Override
     public void addBook(Book book) {
-        System.out.println("(1) 도서번호를 입력해주세요.(유일한 번호) >> ");
-        int bookNum = scanner.nextInt();
-        System.out.println("(2) 도서명을 입력해주세요. >> ");
-        String bookName = scanner.nextLine();
-        System.out.println("(3) 저자명을 입력해주세요. >> ");
-        String name = scanner.nextLine();
-        System.out.println("(4) isbn을 입력해주세요. >> ");
-        System.out.println("(5) 출간일을 입력해주세요.(YYYY-MM-DD형식) >> ");
-        System.out.println("--- 도서 [" + "] 등록이 완료되었습니다. ---");
+        bookList.add(book);
+        System.out.println("--- 도서 [" + book.getName() +  "] 등록이 완료되었습니다. ---");
     }
 
     @Override
@@ -42,11 +36,17 @@ public class BM extends BookManager {
 
     @Override
     public void updateBook(Book book) {
+       /*for (Book b: bookList){
+            if(book.getId().equals(bookList.))
+                userInput = 1;
+        }
 
+      bookList.indexOf(book).setId(book.getId());
+       */
     }
 
     @Override
     public void removeBook(Book book) {
-
+       // bookList.remove(bookList.indexOf(book));
     }
 }
